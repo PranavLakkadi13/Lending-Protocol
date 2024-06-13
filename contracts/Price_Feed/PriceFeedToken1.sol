b6d4c77273a659197ed6f37158536a8e9cfd226d
Pranav <pranavlakkadi@gmail.com>
2024-06-13 17:10:01 +0530

    third commit

---
+++ contracts/Price_Feed/PriceFeedToken1.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+
+contract PriceFeedToken1 is MockV3Aggregator {
+    constructor(uint8 decimal,int256 _initialAnswer) public MockV3Aggregator(decimal,_initialAnswer) {}
+} 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,88 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-foundry");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const RPC_URL_Alchemy_SEPOLIA = process.env.RPC_URL_Alchemy_SEPOLIA;
+const Private_Key = process.env.Private_Key;
+const RPC_URL_POLYGON = process.env.RPC_URL_POLYGON;
+const PolygonScan_API_KEY = process.env.PolygonScan_API_KEY;
+const Etherscan_API_KEY = process.env.Etherscan_API_KEY;
+const Coinmarketcap_API_KEY = process.env.Coinmarketcap_API_KEY;
+const RPC_URL_Alchemy_MAINNET = process.env.RPC_URL_Alchemy_MAINNET;
+
+const Private_Key_G = process.env.Private_Key_G;
+const RPC_URL_G = process.env.RPC_URL_G;
+
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+      // forking: {
+      //   url: RPC_URL_Alchemy_MAINNET,
+      // },
+    },
+    sepolia: {
+      url: RPC_URL_Alchemy_SEPOLIA,
+      accounts: [Private_Key],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    polygon: {
+      url: RPC_URL_POLYGON,
+      accounts: [Private_Key],
+      chainId: 80001,
+      blockConfirmations: 6,
+    },
+  },
+  solidity: {
+    compilers: [
+      { version: "0.4.19" },
+      { version: "0.8.20" },
+      { version: "0.6.6" },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    apiKey: Etherscan_API_KEY,
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://api-sepolia.etherscan.io/api",
+          browserURL: "https://sepolia.etherscan.io",
+        },
+      },
+    ],
+  },
+  polygonscan: {
+    apiKey: PolygonScan_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-reporter.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: Coinmarketcap_API_KEY,
+    token: "ETH",
+  },
+  mocha: {
+    timeout: 1000000,
+  },
 };
