647551f60750a45a2214751b147a366f89f84f80
Pranav <pranavlakkadi@gmail.com>
2024-06-18 11:02:00 +0530

    fifth commit

--- contracts/core/CoreLogic.sol
+++ contracts/core/CoreLogic.sol
@@ -2,12 +2,36 @@
 pragma solidity ^0.8.20;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import  { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { LendTokens } from "./LendTokens.sol";
 
 contract LendingPoolCore {
 
-    IERC20 public immutable i_underlyingAsset;
-    constructor (address token) {
+    IERC20 private immutable i_underlyingAsset;
+    address private immutable i_poolFactory;
+    IERC20 private immutable i_lendingToken;
+    AggregatorV3Interface private immutable i_priceFeed;
+
+    constructor (address token, address pricefeed) {
         i_underlyingAsset = IERC20(token);
+        i_poolFactory = msg.sender;
+        i_lendingToken = IERC20(address(new LendTokens()));
+        i_priceFeed = AggregatorV3Interface(pricefeed); 
+    }
+
+    function assetAddress() external view returns (address) {
+        return address(i_underlyingAsset);
+    }
+
+    function poolFactoryAddress() external view returns (address) {
+        return i_poolFactory;
+    }
+
+    function lendingTokenAddress() external view returns (address) {
+        return address(i_lendingToken);
+    }
+
+    function priceFeedAddress() external view returns (address) {
+        return address(i_priceFeed);
     }
 }
---
+++ contracts/core/LendTokens.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LendTokens is ERC20, ERC20Burnable, Ownable {
+    
+    constructor() 
+    ERC20("TheLendTokens", "LTokens") 
+    Ownable(msg.sender) {}
+
+    function burn(uint256 amount) public override onlyOwner {
+        _burn(msg.sender, amount);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        _mint(account, amount);
+        return true;
+    }
+}
